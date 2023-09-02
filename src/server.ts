72abf5f6724e203a2ae2e7ff987f80b40400f431
Fabrício Mendes <fabricio.jmj@gmail.com>
2023-09-02 00:34:35 -0300

    Setup database connection finished

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,33 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { PostController } from './controllers/post';
 import { Application } from 'express';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpresss();
     this.setupControllers();
+    await this.setupDatabase();
   }
 
-  public getApp(): Application{
+  public getApp(): Application {
     return this.app;
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   private setupExpresss(): void {
     this.app.use(bodyParser.json())
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
   }
 
   private setupControllers(): void {
